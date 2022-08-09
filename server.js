8c311dd6bb46432a4e803c3a64339f76718ebc92
unknown <you@example.com>
2022-08-09 10:50:19 +0300

    added navbar

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
-const express = require('express')
-const path = require('path')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const api = require('./server/routes/api')
+const express = require("express");
+const path = require("path");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const api = require("./server/routes/api");
+mongoose.connect("mongodb://localhost/bitAcademyDB", { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "node_modules")));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mydb', { useNewUrlParser: true })
+app.use("/", api);
+const port = 8888;
 
-const app = express()
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.use(express.static(path.join(__dirname, 'dist')))
-app.use(express.static(path.join(__dirname, 'node_modules')))
-
-
-app.use('/', api)
-
-const port = 8888
-
-app.listen(process.env.PORT || port, function() {
-    console.log(`Runnin runnin and runnin runnin on port ${port}`)
-})
+app.listen(process.env.PORT || port, function () {
+  console.log(`Runnin runnin and runnin runnin on port ${port}`);
+});
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,22 +1,21 @@
-const express = require('express')
-const router = express.Router()
-const Item = require('../models/Item')
+const express = require("express");
+const router = express.Router();
+const Item = require("../models/Item");
 
+router.get("/city/:city", (req, res) => {});
 
-router.get('/city/:city', (req, res) => {
-})
-
-router.get('/get', (req, res) => {
-    Item.find({}, function(err, data) {
-        console.log(data)
-        res.send(data)
-    })
+router.get("/get", (req, res) => {
+  Item.find({}, function (err, data) {
+    console.log(data);
+    res.send(data);
+  });
 });
 
-router.post('/save', (req, res) => {
-    let data = req.body
-    let newItem = new Item(data)
-    newItem.save()
-})
+router.get("/item", (req, res) => []);
+router.post("/save", (req, res) => {
+  let data = req.body;
+  let newItem = new Item(data);
+  newItem.save();
+});
 
-module.exports = router
+module.exports = router;
