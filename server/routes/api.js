d73183326c00de76aaaf764b0635b80542c03075
suha <suhashehahde@gmail.com>
2022-08-09 11:05:03 -0700

    add instert program to db post operation

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const Program = require('../models/Program')
 
 
-
-// router.get('/city/:city', (req, res) => {
-// })
+router.get('/program/:program', (req, res) => {
+    let program = req.body
+    console.log(program);
+})
 
 // router.get('/get', (req, res) => {
 //     Item.find({}, function(err, data) {
@@ -14,10 +15,11 @@ const Program = require('../models/Program')
 //     })
 // });
 
-// router.post('/save', (req, res) => {
-//     let data = req.body
-//     let newItem = new Programm(data)
-//     newItem.save()
-// })
+router.post('/save', (req, res) => {
+    let program = req.body
+    let newProgram = new Program(program)
+    newProgram.save()
+    res.end()  
+})
 
 module.exports = router
